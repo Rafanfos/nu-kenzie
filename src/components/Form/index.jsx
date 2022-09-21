f5ed09d6e57720e5ca070c7709460663d36001ba
Rafanfos <rafaelbsilva555@gmail.com>
2022-09-21 15:56:18 -0300

    feat: add feat/form

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
 import "./style.css";
 
 const Form = ({ listTransactions, setListTransactions }) => {
+  const [description, setDescription] = useState("");
+  const [value, setValue] = useState("");
+  const [type, setType] = useState("");
+
+  const handleTrasactions = (event) => {
+    event.preventDefault();
+    setListTransactions({ description: description, value: value, type: type });
+  };
+
   return (
-    <form>
+    <form onSubmit={handleTrasactions}>
       <label className="grey4" for="description">
         Descrição
       </label>
@@ -10,6 +20,7 @@ const Form = ({ listTransactions, setListTransactions }) => {
         className="grey1"
         placeholder="Digite aqui sua descrição"
         id="description"
+        onChange={(event) => setDescription(event.target.value)}
       ></input>
       <span className="grey50 small">Ex: compra de roupas</span>
       <div className="values">
@@ -18,7 +29,12 @@ const Form = ({ listTransactions, setListTransactions }) => {
             Valor
           </label>
           <div>
-            <input className="grey1" placeholder="1" id="value"></input>
+            <input
+              className="grey1"
+              placeholder="1"
+              id="value"
+              onChange={(event) => setValue(event.target.value)}
+            ></input>
             <span className="grey3">R$</span>
           </div>
         </div>
@@ -26,13 +42,20 @@ const Form = ({ listTransactions, setListTransactions }) => {
           <label className="grey4" for="type">
             Tipo de valor
           </label>
-          <select id="type" className="grey1 grey3">
+          <select
+            id="type"
+            className="grey1 grey3"
+            onChange={(event) => setType(event.target.value)}
+          >
+            <option></option>
             <option className="grey3">Entrada</option>
             <option className="grey3">Saída</option>
           </select>
         </div>
       </div>
-      <button className="primary white">Inserir Valor</button>
+      <button type="submit" className="primary white">
+        Inserir Valor
+      </button>
     </form>
   );
 };
